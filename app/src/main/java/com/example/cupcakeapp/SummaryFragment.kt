f46a41e4089a5e501b02ef3e9740bde2d81c7c82
ivVane <iv.vane@gmail.com>
2023-03-18 01:59:08 +0100

    Added navigation action, created cancel button layouts and click listener, added navigation actions popUpTo attribute and popUpToInclusive attribute.

--- app/src/main/java/com/example/cupcakeapp/SummaryFragment.kt
+++ app/src/main/java/com/example/cupcakeapp/SummaryFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import com.example.cupcakeapp.databinding.FragmentSummaryBinding
 import com.example.cupcakeapp.model.OrderViewModel
 
@@ -38,6 +39,11 @@ class SummaryFragment : Fragment() {
 
     fun sendOrder() {
         Toast.makeText(activity, "Send Order", Toast.LENGTH_SHORT).show()
+    }
+
+    fun cancelOrder() {
+        sharedViewModel.resetOrder()
+        findNavController().navigate(R.id.action_summaryFragment_to_startFragment)
     }
 
     override fun onDestroyView() {
